ecd541d47ef743ff617ced605d33cc5ad1aef2c3
Diyan Lyubchev <diyan.lyubchev@gmail.com>
2019-11-02 19:13:43 +0200

    Added Custom Exeption
    Message View Model 
    Email Component View

---
+++ E-MailApplicationsManager/E-MailApplicationsManager.Service/CustomException/UserExeption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_MailApplicationsManager.Service.CustomException
+{
+    public class UserExeption : Exception
+    {
+        public UserExeption(string masege)
+        : base(String.Format(masege))
+        {
+        }
+    }
+}
---
+++ E-MailApplicationsManager/E-MailApplicationsManager.Web/ViewComponents/EmailViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using E_MailApplicationsManager.Models.Context;
+using E_MailApplicationsManager.Service.Contracts;
+using E_MailApplicationsManager.Service.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_MailApplicationsManager.Web.ViewComponents
+{
+    public class EmailViewComponent : ViewComponent
+    {
+        private readonly IEmailService emailService;
+       // private readonly E_MailApplicationsManagerContext context;
+        public EmailViewComponent(IEmailService emailService)
+        {
+            this.emailService = emailService;
+           // this.context = context;
+        }
+
+
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+           // var db = new EmailService(context);
+            var run = new ConcreteMailService(emailService);
+            run.QuickStart();
+            return View();
+        }
+    }
+}
