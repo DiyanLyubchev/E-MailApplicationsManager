0f0c566530174bbb0bb9607093d2a5da6b5245a4
bobiturbo971@gmail.com <bobiturbo971@gmail.com>
2019-11-03 16:34:05 +0200

    Added AccountController test for HttpPost

---
+++ E-MailApplicationsManager/E-MailApplicationsManager.UnitTests/ControllerTests/AccountController_Should.cs
@@ -0,0 +1,40 @@
+﻿using E_MailApplicationsManager.Service.Contracts;
+using E_MailApplicationsManager.Web.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace E_MailApplicationsManager.UnitTests.ControllerTests
+{
+    [TestClass]
+    public class AccountController_Should
+    {
+        [TestMethod]
+        public void RegisterAccount()
+        {
+            var userWebService = new Mock<IUserService>();
+
+            var defaultContext = new DefaultHttpContext()
+            {
+                User = new ClaimsPrincipal()
+            };
+
+            var controller = new AccountController(userWebService.Object)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = defaultContext
+                }
+            };
+
+            var result = controller.RegisterAccount();
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+    }
+}
