b876a26449e6c4127146e588137a03b345b83714
BoychoDzhorgov <boycho.dzhorgov@gmail.com>
2019-11-07 13:59:51 +0200

    Added new view model.

---
+++ E-MailApplicationsManager/E-MailApplicationsManager.Web/Models/Emails/EmailFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_MailApplicationsManager.Web.Models.Emails
+{
+    public class EmailFormViewModel
+    {
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string EGN { get; set; }
+    }
+}
