9558941e790724920331a0f263476d2d877e6b55
Diyan Lyubchev <diyan.lyubchev@gmail.com>
2019-11-07 17:25:05 +0200

    Added LoanApplicantDto and .js function for  form

---
+++ E-MailApplicationsManager/E-MailApplicationsManager.Service/Dto/LoanApplicantDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_MailApplicationsManager.Service.Dto
+{
+    public class LoanApplicantDto
+    {
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string EGN { get; set; }
+
+        public string userId { get; set; }
+    }
+}
