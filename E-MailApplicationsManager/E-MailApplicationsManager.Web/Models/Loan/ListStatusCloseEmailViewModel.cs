aa06bddcc69b81790673fd509423c578fc60640c
Diyan Lyubchev <diyan.lyubchev@gmail.com>
2019-11-13 20:34:29 +0200

    Added method for list all close status emails and test
    Fixed bugs

---
+++ E-MailApplicationsManager/E-MailApplicationsManager.Web/Models/Loan/ListStatusCloseEmailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using E_MailApplicationsManager.Models.Model;
+using System.Collections.Generic;
+
+namespace E_MailApplicationsManager.Web.Models.Loan
+{
+    public class ListStatusCloseEmailViewModel
+    {
+        public List<StatusCloseEmailViewModel> Emails { get; set; }
+
+        public ListStatusCloseEmailViewModel(IEnumerable<LoanApplicant> closeStatusEmails)
+        {
+            this.Emails = new List<StatusCloseEmailViewModel>();
+            foreach (var closeStatusEmail in closeStatusEmails)
+            {
+                this.Emails.Add(new StatusCloseEmailViewModel(closeStatusEmail));
+            }
+        }
+    }
+}
---
+++ E-MailApplicationsManager/E-MailApplicationsManager.Web/Models/Loan/StatusCloseEmailViewModel.cs
@@ -0,0 +1,37 @@
+﻿using E_MailApplicationsManager.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_MailApplicationsManager.Web.Models.Loan
+{
+    public class StatusCloseEmailViewModel
+    {
+        public StatusCloseEmailViewModel(LoanApplicant loanApplicant)
+        {
+            this.Id = loanApplicant.Id;
+            this.ClienName = loanApplicant.Name;
+            this.PhoneNumber = loanApplicant.PhoneNumber;
+            this.GmailId = loanApplicant.GmailId;
+        }
+
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required]   
+        public string ClienName { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string GmailId { get; set; }
+
+        [Required]
+        public string EmployeeName { get; set; }
+
+        public DateTime? Date { get; set; }
+    }
+}
