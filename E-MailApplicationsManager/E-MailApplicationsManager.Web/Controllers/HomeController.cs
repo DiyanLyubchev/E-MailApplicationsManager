2c7c25c6de487c162d61437f532a1af73cd54daa
BoychoDzhorgov <boycho.dzhorgov@gmail.com>
2019-11-20 22:27:49 +0200

    working on homepage

--- E-MailApplicationsManager/E-MailApplicationsManager.Web/Controllers/HomeController.cs
+++ E-MailApplicationsManager/E-MailApplicationsManager.Web/Controllers/HomeController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_MailApplicationsManager.Models.Common;
+using E_MailApplicationsManager.Models.Model;
+using E_MailApplicationsManager.Service.Contracts;
+using E_MailApplicationsManager.Web.Models.Emails;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace E_MailApplicationsManager.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ISearchService searchService;
+
+        public HomeController(ISearchService searchService)
+        {
+            this.searchService = searchService;
+        }
 
         public IActionResult Index()
         {
@@ -12,6 +26,17 @@ namespace E_MailApplicationsManager.Web.Controllers
                 return LocalRedirect("~/identity/account/login");
             }
             return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetChartData()
+        {
+            var list = await this.searchService.GetEmailsForChart();
+        
+            var viewModel = new ListEmailsFromChartViewModel(list);
+           
+            return Json(viewModel);
         }
 
     }
---
+++ E-MailApplicationsManager/E-MailApplicationsManager.Web/wwwroot/js/index.js
@@ -0,0 +1,31 @@
+﻿window.onload = function () {
+    $.ajax({
+        url: '/Home/GetChartData',
+        type: 'GET',
+        dataType: 'json',
+        cache: false,
+        success: function (result) {
+            console.log(result);
+            var ctx = document.getElementById('myChart').getContext('2d');
+            new Chart(ctx, {
+                type: 'polarArea',
+                data: {
+                    labels: result.email.map(element => element.EmailStatus),  
+                    datasets: [{
+                        label: 'My First dataset',
+                        backgroundColor: result.element.map(element => {
+                            var colorNumber = 255 - 255 / (element.id + 1);
+                            return `rgb(${255},${colorNumber},${colorNumber})`
+                        }),
+                        borderColor: result.element.map(element => {
+                            var colorNumber = 255 - 255 / (element.id + 1);
+                            return `rgb(${255},${colorNumber},${colorNumber})`
+                        }),
+                        data: result.emails.map(element => element.emails.StatusId)
+                    }]
+                },
+                options: {}
+            });
+        }
+    });
+};
