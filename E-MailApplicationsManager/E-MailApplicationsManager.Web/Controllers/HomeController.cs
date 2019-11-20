c48f56d96269951b9da72c575b4d65307f86ef6e
BoychoDzhorgov <boycho.dzhorgov@gmail.com>
2019-11-20 16:48:03 +0200

    Working on chart for the emails.

--- E-MailApplicationsManager/E-MailApplicationsManager.Web/Controllers/HomeController.cs
+++ E-MailApplicationsManager/E-MailApplicationsManager.Web/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_MailApplicationsManager.Models.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace E_MailApplicationsManager.Web.Controllers
 {
@@ -12,6 +16,57 @@ namespace E_MailApplicationsManager.Web.Controllers
                 return LocalRedirect("~/identity/account/login");
             }
             return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetChartData()
+        {
+            var emailStatus = new EmailStatus();
+            emailStatus.Id = 0;
+            emailStatus.Status = "testStatus";
+            emailStatus.Emails = new List<Email>();
+            emailStatus.Emails.Add(new Email());
+            emailStatus.Emails.Add(new Email());
+            emailStatus.Emails.Add(new Email());
+            emailStatus.Emails.Add(new Email());
+            emailStatus.Emails.Add(new Email());
+            emailStatus.Emails.Add(new Email());
+
+            var emailStatus2 = new EmailStatus();
+            emailStatus2.Id = 1;
+            emailStatus2.Status = "testStatus2";
+            emailStatus2.Emails = new List<Email>();
+            emailStatus2.Emails.Add(new Email());
+            emailStatus2.Emails.Add(new Email());
+
+            var emailStatus3 = new EmailStatus();
+            emailStatus3.Id = 2;
+            emailStatus3.Status = "testStatus";
+            emailStatus3.Emails = new List<Email>();
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+            emailStatus3.Emails.Add(new Email());
+
+
+            var list = new List<EmailStatus>();
+            list.Add(emailStatus);
+            list.Add(emailStatus2);
+            list.Add(emailStatus3);
+            return Json(list);
         }
 
     }
---
+++ E-MailApplicationsManager/E-MailApplicationsManager.Web/wwwroot/js/index.js
@@ -0,0 +1,31 @@
+﻿window.onload = function () {
+    $.ajax({
+        url: '/Home/GetChartData',
+        type: 'GET',
+        dataType: 'json',
+        cache: false,
+        success: function (result) {
+            console.log(result);
+            var ctx = document.getElementById('myChart').getContext('2d');
+            new Chart(ctx, {
+                type: 'polarArea',
+                data: {
+                    labels: result.map(element => element.status),
+                    datasets: [{
+                        label: 'My First dataset',
+                        backgroundColor: result.map(element => {
+                            var colorNumber = 255 - 255 / (element.id + 1);
+                            return `rgb(${255},${colorNumber},${colorNumber})`
+                        }),
+                        borderColor: result.map(element => {
+                            var colorNumber = 255 - 255 / (element.id + 1);
+                            return `rgb(${255},${colorNumber},${colorNumber})`
+                        }),
+                        data: result.map(element => element.emails.length)
+                    }]
+                },
+                options: {}
+            });
+        }
+    });
+};
